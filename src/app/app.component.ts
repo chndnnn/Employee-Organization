9f700f973c1827511a64fd1a1ec3f95bdccbf34a
chndnnn <chndn136@gmail.com>
2024-02-07 21:53:03 -0800

    nav commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import { NavComponent } from "./Components/nav/nav.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, MatButtonModule, MatIconModule, NavComponent]
 })
 export class AppComponent {
   title = 'Employee_Organization';
